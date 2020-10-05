aa5213d650a60d008329d2b82b3a2b6653581456
cctorres <c.torresalzate@gmail.com>
2020-10-04 22:28:46 -0500

    wcat done

---
+++ enunciado/wcat/wcat.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    if (argc == 1)
+    {
+        printf("wcat: file1 [file2 ...]\n");
+        //return 0;
+        return 1;
+    }
+    for (int i = 1; i < argc; i++)
+    {
+        FILE *currentFile = fopen(argv[i], "r");
+        if (currentFile == NULL)
+        {
+            printf("wcat: cannot open file\n");
+            return 1;
+        }
+        char character = fgetc(currentFile);
+        while (character != EOF)
+        {
+            printf("%c",character);
+            character = fgetc(currentFile);    
+        }  
+          
+        fclose(currentFile);
+    }
+    printf("\n");
+    return 0;
+}
